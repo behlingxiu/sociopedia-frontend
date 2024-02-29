5a3bfa9717a59fec05171f4eb37d3dbb6f518df4
behlingxiu <122595340+behlingxiu@users.noreply.github.com>
2024-02-29 12:39:43 +0800

    Update Form.jsx

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -233,6 +233,7 @@ const Form = () => {
               onChange={handleChange}
               value={values.email}
               name="email"
+              placeHolder="testing123@gmail.com"
               error={Boolean(touched.email) && Boolean(errors.email)}
               helperText={touched.email && errors.email}
               sx={{ gridColumn: "span 4" }}
@@ -244,6 +245,7 @@ const Form = () => {
               onChange={handleChange}
               value={values.password}
               name="password"
+              placeHolder="12345678"
               error={Boolean(touched.password) && Boolean(errors.password)}
               helperText={touched.password && errors.password}
               sx={{ gridColumn: "span 4" }}
